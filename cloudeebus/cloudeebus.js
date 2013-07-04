4019eec27b235949585f3e4925962a1752109eed
Luc Yriarte <luc.yriarte@intel.com>
2013-07-04 10:36:18 +0200

    error.uri is autobahn-related, not needed here

--- cloudeebus/cloudeebus.js
+++ cloudeebus/cloudeebus.js
@@ -45,12 +45,8 @@ cloudeebus.log = function(msg) {
 };
 
 cloudeebus.getError = function(error) {
-	if (error.desc && error.uri)
-		return error.desc + " : " + error.uri;
 	if (error.desc)
 		return error.desc;
-	if (error.uri)
-		return error.uri;
 	if (error.name && error.message)
 		return error.name + " : " + error.message;
 	if (error.message)

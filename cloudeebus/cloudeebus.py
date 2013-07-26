f1de45e9f5c46c58dd9dacc8280b271aa21ed1c9
Frederic PAUT <frederic.paut@linux.intel.com>
2013-07-26 18:34:14 +0200

    dbus service : small fix Instanciate class by passing object path as string (instead of reference)

--- cloudeebus/cloudeebus.py
+++ cloudeebus/cloudeebus.py
@@ -629,7 +629,7 @@ class CloudeebusService:
 
         ## Class already exist, instanciate it if not already instanciated
         if (self.serviceAgents.has_key(className) == False):
-            self.serviceAgents[className] = eval(className + "(self.bus, callback=self.srvCB, objPath=agentObjectPath, busName=self.srvName)", self.globalCtx, self.localCtx)
+            self.serviceAgents[className] = eval(className + "(self.bus, callback=self.srvCB, objPath='"+agentObjectPath+"', busName=self.srvName)", self.globalCtx, self.localCtx)
             
         self.serviceAgents[className].add_to_connection()
         return (agentObjectPath)
